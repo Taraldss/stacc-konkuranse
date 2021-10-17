dea5e88dc43c26103d63ba1ac05063ec30a765e1
Tarald Skaar <taraldskaar@Taralds-MBP.home>
2021-10-17 19:53:08 +0200

    No functionality, only listing of nft's

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home({ nftArray }) {
   return (
@@ -7,27 +7,34 @@ export default function Home({ nftArray }) {
       <Head>
         <title>Eneftee</title>
       </Head>
-      {nftArray.map(nft => (
-        <div key={nft.id}>
-          <a>
-            <h3>{nft.name}</h3>
-            <img src={nft.image_url} />
-          </a>
-        </div>
-      ))}
+    <p>This page is under construction, as of this moment you can only look at these nft's</p>
+
+      <ul>
+        {nftArray.map((nft) => (
+          nft.name !== null ? (
+            <li key={nft.id}>
+            <a>
+              <h3>{nft.name}</h3>
+              <img src={nft.image_url} />
+              <p>{}</p>
+            </a>
+          </li>
+          ) : null
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export const getStaticProps = async () =>{
-
-  const res = await fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20')
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20"
+  );
   const data = await res.json();
 
   return {
-
     props: {
-      nftArray: data.assets
-    }
-  }
-}
+      nftArray: data.assets,
+    },
+  };
+};
